7483f0bf6e8b9f84ce68da05f33c3ec7214122e8
arkadacasinos <bezdep.rf@mail.ru>
2026-05-01 15:54:14 +0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -165,9 +165,7 @@ export default function RootLayout({
   return (
     <html lang="ru" className={`${inter.variable} ${unbounded.variable} bg-background scroll-smooth`}>
       <head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link rel="dns-prefetch" href="https://fonts.googleapis.com" />
+
         <meta name="format-detection" content="telephone=no" />
         <meta name="mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
